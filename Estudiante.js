46a0d2d45ab4496957c9a1255481335ce52f74fc
cristianhar <yohan4752@gmail.com>
2024-08-04 17:50:53 -0500

    Herencia - Implementación de Propiedad adicional para el grado por [Nombre]

--- Estudiante.js
+++ Estudiante.js
@@ -1,7 +1,14 @@
 const Persona = require('./Persona');
 
 class Estudiante extends Persona {
+    #grado;
 
+    constructor(nombre, edad, fechaNacimiento, grado) {
+        super(nombre, edad, fechaNacimiento);
+        this.#grado = grado;
+    }
+
+ 
 }
 
 module.exports = Estudiante;
