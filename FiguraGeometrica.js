daffda11d00a57884253c3ec0d9ccc30850b8286
cristianhar <yohan4752@gmail.com>
2024-08-04 18:06:32 -0500

    Encapsulamiento y Herencia - Implementación de Subclase Rectangulo por cristianhar

--- FiguraGeometrica.js
+++ FiguraGeometrica.js
@@ -31,3 +31,47 @@ class FiguraGeometrica {
         return this.#color;
     }
 }
+
+
+class Rectangulo extends FiguraGeometrica {
+    #longitud;
+    #ancho;
+
+    constructor(nombre, color, longitud, ancho) {
+        super(nombre, color);
+        this.#longitud = longitud;
+        this.#ancho = ancho;
+    }
+
+    establecerLongitud(longitud) {
+        if (typeof longitud === 'number' && longitud > 0) {
+            this.#longitud = longitud;
+        } else {
+            throw new Error('La longitud debe ser un número positivo.');
+        }
+    }
+
+    obtenerLongitud() {
+        return this.#longitud;
+    }
+
+    establecerAncho(ancho) {
+        if (typeof ancho === 'number' && ancho > 0) {
+            this.#ancho = ancho;
+        } else {
+            throw new Error('El ancho debe ser un número positivo.');
+        }
+    }
+
+    obtenerAncho() {
+        return this.#ancho;
+    }
+
+    calcularArea() {
+        return this.#longitud * this.#ancho;
+    }
+
+    calcularPerimetro() {
+        return 2 * (this.#longitud + this.#ancho);
+    }
+}
