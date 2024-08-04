946730993ebebc5da42f500466aecab094fa9477
cristianhar <yohan4752@gmail.com>
2024-08-04 17:50:31 -0500

    Herencia - Implementación de Clase Estudiante por cristianhar

--- Estudiante.js
+++ Estudiante.js
@@ -1,12 +1,7 @@
 const Persona = require('./Persona');
 
 class Estudiante extends Persona {
-    #grado;
 
-    constructor(nombre, edad, fechaNacimiento, grado) {
-        super(nombre, edad, fechaNacimiento);
-        this.#grado = grado;
-    }
 }
 
 module.exports = Estudiante;
