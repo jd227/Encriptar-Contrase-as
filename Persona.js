b7e4619c6e17e38ea634c81ac11bb4eb1ff2e424
cristianhar <yohan4752@gmail.com>
2024-08-04 17:22:06 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por cristianhar

--- Persona.js
+++ Persona.js
@@ -8,6 +8,30 @@ class Persona {
         this.#edad = edad;
         this.#fechaNacimiento = fechaNacimiento;
     }
+
+    establecerNombre(nombre) {
+        this.#nombre = nombre;
+    }
+
+    obtenerNombre() {
+        return this.#nombre;
+    }
+
+    establecerEdad(edad) {
+        this.#edad = edad;
+    }
+
+    obtenerEdad() {
+        return this.#edad;
+    }
+
+    establecerFechaNacimiento(fechaNacimiento) {
+        this.#fechaNacimiento = fechaNacimiento;
+    }
+
+    obtenerFechaNacimiento() {
+        return this.#fechaNacimiento;
+    }
 }
 
 module.exports = Persona;
