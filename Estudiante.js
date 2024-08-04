64b9fa2a14a32c0631323ba2f3e86ebe90e5fd1a
cristianhar <yohan4752@gmail.com>
2024-08-04 17:54:30 -0500

    Herencia - Pruebas de implementación de la clase Estudiante por cristianhar

--- Estudiante.js
+++ Estudiante.js
@@ -25,4 +25,12 @@ class Estudiante extends Persona {
     }
 }
 
+const prueba = new Estudiante('Cristian Arboleda', 26, '1998-05-01', '5 Trimestre');
+console.log(prueba.obtenerNombre());                  // Cristian Arboleda
+console.log(prueba.obtenerEdad());                    // 26
+console.log(prueba.obtenerFechaNacimiento());         // 1998-05-01
+console.log(prueba.obtenerGrado());                   // 5 Trimestre
+// Salida esperada:
+// Cristian Arboleda - Edad: 26 - Fecha de Nacimiento: 1998-05-01 - Grado: 5 Trimestre
+
 module.exports = Estudiante;
